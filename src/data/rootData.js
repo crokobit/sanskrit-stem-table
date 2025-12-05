b359f230468fd6f5772859841a3ce08f8ef8e797
crokobit <crokobit@gmail.com>
2025-12-05 12:45:45 +0800

    feat: update Sanskrit root data

--- src/data/rootData.js
+++ src/data/rootData.js
@@ -22,7 +22,31 @@ export const ROOT_DATA = {
     3: { id: 'root_3', label: 'Class 3', description: 'Class 3 Verbs', items: [] },
     4: { id: 'root_4', label: 'Class 4', description: 'Class 4 Verbs', items: [] },
     5: { id: 'root_5', label: 'Class 5', description: 'Class 5 Verbs', items: [] },
-    6: { id: 'root_6', label: 'Class 6', description: 'Class 6 Verbs', items: [] },
+    6: {
+        id: 'root_6',
+        label: 'Class 6',
+        description: '第六類動詞特例 陳主',
+        items: [
+            {
+                description: '不規則動詞',
+                exceptions: [
+                    { root: { value: '√iṣ', type: '6P', def: '希求，许' }, form: 'icchati' },
+                    { root: { value: '√prach', type: '6P', def: '问' }, form: 'pṛcchati' }
+                ]
+            },
+            {
+                description: '有些第 6 类动词变位时，在词根尾音前插入同类的鼻音。例如，尾音是腭音(c、ch、j、jh)，则插入ñ;尾音是齿音(t、th、d、dh)，则插入n ;尾音是唇音(p、ph、b、bh)， 则插入 m。尾音是咝音(ś、ṣ、s)或 h，则插入 ṃ(在写本中，ṃ 和鼻音字母常替换使用。 亦即所有辅音字母前要加鼻音时，都可能写成 ṃ)。常见的有以下几个:',
+                exceptions: [
+                    { root: { value: '√sic', type: '6U', def: '滴，使...变湿' }, form: 'siñcati' },
+                    { root: { value: '√muc', type: '6U', def: '释放，解脱' }, form: 'muñcati' },
+                    { root: { value: '√lup', type: '6U', def: '毁坏' }, form: 'lumpati' },
+                    { root: { value: '√kṛt', type: '6P', def: '割，砍' }, form: 'kṛntati' },
+                    { root: { value: '√vid', type: '6U', def: '发现，找到' }, form: 'vindati' },
+                    { root: { value: '√lip', type: '6U', def: '涂' }, form: 'limpati' }
+                ]
+            }
+        ]
+    },
     7: { id: 'root_7', label: 'Class 7', description: 'Class 7 Verbs', items: [] },
     8: { id: 'root_8', label: 'Class 8', description: 'Class 8 Verbs', items: [] },
     9: { id: 'root_9', label: 'Class 9', description: 'Class 9 Verbs', items: [] },
